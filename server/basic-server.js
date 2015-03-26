d02a60955968435849fd294ab63296722b933f45
MKS-Student <mks.student@hackreactor.com>
2015-03-25 19:43:15 -0500

    tests passing

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,6 +1,6 @@
 /* Import node's http module: */
 var http = require("http");
-
+var handleRequest = require('./request-handler.js')
 
 // Every server needs to listen on a port with a unique number. The
 // standard port for HTTP servers is port 80, but that port is
@@ -17,24 +17,22 @@ var ip = "127.0.0.1";
 
 
 // We use node's http module to create a server.
-//
+
 // The function we pass to http.createServer will be used to handle all
 // incoming requests.
-//
+
 // After creating the server, we will tell it to listen on the given port and IP. */
-var server = http.createServer(handleRequest);
+var server = http.createServer(handleRequest.requestHandler);
 console.log("Listening on http://" + ip + ":" + port);
 server.listen(port, ip);
 
 // To start this server, run:
-//
 //   node basic-server.js
-//
 // on the command line.
-//
+
 // To connect to the server, load http://127.0.0.1:3000 in your web
 // browser.
-//
+
 // server.listen() will continue running as long as there is the
 // possibility of serving more requests. To stop your server, hit
 // Ctrl-C on the command line.
